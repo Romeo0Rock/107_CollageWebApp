a8ddcf2d377a3e41a615f402d524fa980977c429
Romeo0Rock <116769162+Romeo0Rock@users.noreply.github.com>
2023-06-05 20:06:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var speechRecognition = window.webkitSpeechRecognition;
+
+var content
+
+
+
+
+setTimeout(function){
+   img_id = "selfie1";
+   take_snapshot();
+   speak_data = "Taking your selfie in 10 seconds";
+   var utterThis = SpeechSynthesisUtterance(speak_data);
+}
